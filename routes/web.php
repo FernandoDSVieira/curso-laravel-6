e4228d4077565ed8847e102c0cd83e841d36fc41
FernandoDSVieira <fernandoytgamer@gmail.com>
2023-06-06 14:37:36 -0300

    Add route redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::any('/any', function() {
+    return 'Any';
+});
+
+Route::match(['get', 'post'], '/match', function() {
+    return 'match funcionando';
+});
+
+Route::get('/categorias/{flag}/teste', function($flag) {
+    return "posts da categoria: {$flag}";
+});
+
+Route::get('/produtos/{idProduct?}', function($idProduct = '') {
+    return "Produtos(s) {$idProduct}";
+});
+
+Route::post('/register', function () {
+    return '';
+});
+
+Route::get('/empresa', function () {
+    return 'Informações sobre a empresa';
+});
+
+Route::get('/contato', function () {
+    return view('site.contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
