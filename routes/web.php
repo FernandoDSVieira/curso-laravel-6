bfdddb584a1e80a77d129fc9c09165db29244fe8
FernandoDSVieira <fernandoytgamer@gmail.com>
2023-06-12 14:45:02 -0300

    Rotas com parametros

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get("redirect1", function() {
+//     return redirect('/redirect2');
+// });
+
+Route::get("redirect2", function() {
+    return 'Redirect 02';
+});
+
+
 Route::any('/any', function() {
     return 'Any';
 });
